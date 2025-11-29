89e38086e0cd2e6b996d84afb73f857cf227cca5
victordanielrb <victordanielramosb@gmail.com>
2025-11-29 06:24:56 +0530

    fix: fixes the refreshPool workflow to dont use dist and incorrect files

--- dist/interfaces/general.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Blockchain = void 0;
-var Blockchain;
-(function (Blockchain) {
-    Blockchain["SUI"] = "SUI";
-    Blockchain["EVM"] = "EVM";
-    Blockchain["SOLANA"] = "SOLANA";
-})(Blockchain || (exports.Blockchain = Blockchain = {}));
